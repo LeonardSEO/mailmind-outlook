807c78dc88edc87bf54e70cc3ea054da6bf3f8b2
LeonardSEO <tactile.tatamis_0x@icloud.com>
2025-01-31 07:49:30 +0100

    Fix Office.js initialization and remove service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { initializeIcons } from '@fluentui/react/lib/Icons';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
-import './styles.less';
 import '@fluentui/react/dist/css/fabric.min.css';
 
 initializeIcons();
 
 let isOfficeInitialized = false;
 
-const title = 'outlook-addin-using-react-demo';
+const title = 'MailMind';
 
 const container = document.getElementById('container');
 const root = ReactDOM.createRoot(container!);
@@ -24,13 +22,11 @@ const render = (Component: typeof App) => {
     );
 };
 
+/* Initial render showing a progress bar */
+render(App);
+
 /* Render application after Office initializes */
 Office.initialize = () => {
     isOfficeInitialized = true;
     render(App);
 };
-
-/* Initial render showing a progress bar */
-render(App);
-
-registerServiceWorker();
